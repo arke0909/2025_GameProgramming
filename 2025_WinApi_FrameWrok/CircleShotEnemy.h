446f609430e4a9a9cf0715550c9d11c06c25d95a
kimjinho <russiakim2008@gmail.com>
2025-12-11 00:07:38 +0900

    feat : circle shot enemy none move enemy combined

--- 2025_WinApi_FrameWrok/CircleShotEnemy.h
+++ 2025_WinApi_FrameWrok/CircleShotEnemy.h
@@ -1,11 +1,17 @@
 #pragma once
 #include "Enemy.h"
+#include "WindowManager.h"
+#include "Window.h"
+
 class CircleShotEnemy :
     public Enemy
 {
 public:
     CircleShotEnemy();
 	~CircleShotEnemy() override = default;
+
+public:
+    void CreateEnemyWindow();
 
 private:
     void Update() override;
@@ -14,5 +20,6 @@ private:
 private:
     float _coolTime;
 	float _accTime;
-};
 
+    Window* _window;
+};
