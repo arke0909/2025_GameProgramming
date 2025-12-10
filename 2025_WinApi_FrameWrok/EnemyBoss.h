f83d6af633c58c92af2f87ff2c8f53f9eea65f41
kimjinho <russiakim2008@gmail.com>
2025-12-10 10:36:41 +0900

    refactor : enemy boss

--- 2025_WinApi_FrameWrok/EnemyBoss.h
+++ 2025_WinApi_FrameWrok/EnemyBoss.h
@@ -6,8 +6,7 @@
 #include "Window.h"
 #include <vector>
 
-class EnemyBoss :
-    public Enemy
+class EnemyBoss : public Enemy
 {
 public:
     EnemyBoss();
@@ -20,14 +19,16 @@ public:
     void ChangeState(std::string state);
     void EnterCollision(Collider* _other);
 
-    int GetCurrentPhase() const { return _currentPhase; } // 현재 페이지
-    PhaseData GetCurrentPhaseData() const; // 현재 페이지의 데이터
+    int GetCurrentPhase() const { return _currentPhase; }
+    PhaseData GetCurrentPhaseData() const;
 
-    //스킬들
     void ExecuteCircleShot(const PatternData& pattern);
     void ExecuteLinearShot(const PatternData& pattern);
     void ExecuteBounceShot(const PatternData& pattern);
     void ExecuteTrackingShot(const PatternData& pattern);
+    void ExecuteMoveSprayShot(const PatternData& pattern);
+    void ExecuteCircularSector(const PatternData& pattern);
+    void ExecuteDistanceMoveSkill();
 
     Player* GetTarget() const { return _player; }
 
@@ -35,9 +36,22 @@ private:
     void InitializePhases();
     void CheckPhaseTransition();
     void TransitionToPhase(int phase);
+
     Window* _window;
-
     int _currentPhase;
-
     std::vector<PhaseData> _phases;
+
+    const float _minDistance = 200.0f;
+    const float _maxDistance = 390.0f;
+    const float _moveStep = 80.0f;
+
+    bool _isMoving = false;
+    Vec2 _moveTarget;
+    float _moveSpeed = 600.f;
+
+    bool _isSprayMoving = false;
+    float _sprayTime = 0.f;
+    float _sprayDuration = 0.8f;
+    float _sprayInterval = 0.1f;
+    float _sprayAccTime = 0.f;
 };
