c2951e6fa4b6676a29a06786db16e660dab64175
kimjinho <russiakim2008@gmail.com>
2025-12-10 20:07:17 +0900

    Revert "fix : change state error"
    This reverts commit bee208106a7f347ccbd9aa18054d03a1109278de.

--- 2025_WinApi_FrameWrok/EnemyMeleeAttackState.cpp
+++ 2025_WinApi_FrameWrok/EnemyMeleeAttackState.cpp
@@ -4,10 +4,9 @@
 
 EnemyMeleeAttackState::EnemyMeleeAttackState(Enemy* owner, std::wstring animationName)
 	: EnemyState(owner, L"ATTACK"),
-	_coolTime(1.5f),
-	_currentTime(1.5f)
+	_coolTime(2.5f),
+	_currentTime(0.0f)
 {
-	_attackRange = owner->GetAttackRange();
 }
 
 EnemyMeleeAttackState::~EnemyMeleeAttackState()
@@ -18,29 +17,16 @@ void EnemyMeleeAttackState::Enter()
 {
 	EnemyState::Enter();
 	_isAttack = true;
+	cout << "Melee Attack State Entered" << endl;
 }
 
 void EnemyMeleeAttackState::Update()
 {
 	EnemyState::Update();
-
-	Vec2 enemyPos = _enemy->GetPos();
-	Vec2 targetPos = _enemy->GetTarget()->GetPos();
-
-	float dx = targetPos.x - enemyPos.x;
-	float dy = targetPos.y - enemyPos.y;
-	float dist = sqrtf(dx * dx + dy * dy);
-
-	if (dist > _attackRange)
-	{
-		_enemy->ChangeState("MOVE");
-		return;
-	}
-
 	if (_isAttack) {
 		_currentTime += fDT;
 	}
-	s
+
 	if (_coolTime <= _currentTime) {
 		Attack();
 		_currentTime = 0.0f;
--- 2025_WinApi_FrameWrok/EnemyMeleeAttackState.h
+++ 2025_WinApi_FrameWrok/EnemyMeleeAttackState.h
@@ -17,7 +17,6 @@ public:
 private:
     float _coolTime;
     float _currentTime;
-	float _attackRange;
 
 	bool _isAttack;
 
